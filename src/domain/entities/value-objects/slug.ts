3d9e146bf9fe6f5e112266423e30c7e90ebb9e13
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-06-26 11:43:17 -0300

    feat: fundamentals of Domain-Driven Design

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,30 @@
+export class Slug {
+    public value: string
+
+
+    constructor(value: string){
+        this.value = value
+    }
+
+
+    /**
+     * Receives a string and normalize it as a slug.
+     * 
+     * Example: "An example title" => "an-example-title"
+     * 
+     * @param text {string}
+     */
+    static createFromText(text: string){
+        const slugText = text
+            .normalize("NFKD") //Devolve a string sem acentuação
+            .toLowerCase() //String em minusculo
+            .trim() //String sem espaçamento tanto na direita e na esquerda
+            .replace(/\s+/g, '-')
+            .replace(/[^\w-]+/g, '')
+            .replace(/_/g, '-')
+            .replace(/--+/g, '-')
+            .replace(/-$/g, '')
+
+        return new Slug(slugText)
+    }
+}
---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,27 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import type { AnswerRepository } from "../../repositories/answers-repository"
+import { Answer } from "../entities/answer"
+
+interface AnswerQuestionUseCaseRequest {
+    instructorId: string,
+    questionId: string
+    content: string
+}
+
+export class AnswerQuestionUseCase {
+    constructor(
+        private answersRepository: AnswerRepository
+    ){}
+
+    async execute({instructorId, questionId, content}: AnswerQuestionUseCaseRequest){
+        const answer = Answer.create({
+            content,
+            authorId: new UniqueEntityID(instructorId),
+            questionId: new UniqueEntityID(questionId)
+        })
+
+        await this.answersRepository.create(answer)
+
+        return answer
+    }
+}
